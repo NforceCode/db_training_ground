2cade824f94aec7b6b17f78e2cd102a7ee3e603a
Kyrylo Rozumovskyi <nortforce@gmail.com>
2021-03-19 10:03:22 +0200

    Refactor SQL identifiers

--- Beta_Cluster.session.sql
+++ Beta_Cluster.session.sql
@@ -2,19 +2,19 @@
 DROP TABLE users;
 
 CREATE TABLE users (
-  id serial PRIMARY KEY,
-  first_name varchar(64) NOT NULL ,
-  last_name varchar(64) NOT NULL ,
-  email varchar(256) NOT NULL UNIQUE CHECK (email != ''),
-  isMale boolean NOT NULL,
-  birthday date NOT NULL CHECK (birthday < current_date AND birthday > '1900-1-1'),
-  height numeric(3,2) NOT NULL CHECK (height > 0.2 AND height < 3),
+  "id" serial PRIMARY KEY,
+  "first_name" varchar(64) NOT NULL ,
+  "last_name" varchar(64) NOT NULL ,
+  "email" varchar(256) NOT NULL UNIQUE CHECK ("email" != ''),
+  "isMale" boolean NOT NULL,
+  "birthday" date NOT NULL CHECK ("birthday" < current_date AND "birthday" > '1900-1-1'),
+  "height" numeric(3,2) NOT NULL CHECK ("height" > 0.2 AND "height" < 3),
   weight numeric(5,2) NOT NULL CHECK (weight > 0 AND weight < 500),
-  CONSTRAINT "UNQ_FULL_NAME" UNIQUE (first_name, last_name),
-  CONSTRAINT "CK_FULL_NAME" CHECK (first_name !='' AND last_name !='')
+  CONSTRAINT "UNQ_FULL_NAME" UNIQUE ("first_name", "last_name"),
+  CONSTRAINT "CK_FULL_NAME" CHECK ("first_name" !='' AND "last_name" !='')
 );
 
-INSERT INTO users (first_name, last_name, email, isMale, birthday, height, weight) VALUES 
+INSERT INTO users ("first_name", "last_name", "email", "isMale", "birthday", "height", weight) VALUES 
 ('Test', 'Testovich', 'test@test.test', true, '1991-12-21', 1.95, 100),
 ('Archaon', 'the Everchosen', 'burntheoldworld@chaos.net', true, '1964-6-6', 2.15, 10.45),
 ('Tester', 'the Evertesting', 'tester@test.test', true, '1974-6-6', 2.15, 100),
@@ -25,22 +25,22 @@ SELECT * FROM users ORDER BY id ASC;
 */
 
 CREATE TABLE content (
-  id serial PRIMARY KEY,
-  name varchar(128) NOT NULL CHECK (name != ''),
-  owner_id int, 
-  created_at timestamp DEFAULT current_timestamp,
+  "id" serial PRIMARY KEY,
+  "name" varchar(128) NOT NULL CHECK ("name" != ''),
+  "owner_id" int, 
+  "created_at" timestamp DEFAULT current_timestamp,
   "desc" varchar(1024) NOT NULL CHECK ("desc" != '')
-  CONSTRAINT "FK_owner_id" FOREIGN KEY (owner_id) REFERENCES users (id)
+  CONSTRAINT "FK_owner_id" FOREIGN KEY ("owner_id") REFERENCES users (id)
 );
 
-INSERT INTO content (name, owner_id, "desc") VALUES('Hello World', 1, 'My first content Pog');
+INSERT INTO content ("name", "owner_id", "desc") VALUES('Hello World', 1, 'My first content Pog');
 
 CREATE TABLE user_reaction (
-  user_id int NOT NULL CONSTRAINT "FK_user_id" FOREIGN KEY (user_id) REFERENCES users (id),
-  content_id int NOT NULL CONSTRAINT "FK_content_id" FOREIGN KEY (content_id) REFERENCES content (id),
-  isLiked boolean
+  "user_id" int NOT NULL CONSTRAINT "FK_user_id" FOREIGN KEY ("user_id") REFERENCES users (id),
+  "content_id" int NOT NULL CONSTRAINT "FK_content_id" FOREIGN KEY ("content_id") REFERENCES content (id),
+  "isLiked" boolean
   
 );
 
-INSERT INTO user_reaction (user_id, content_id, isLiked) VALUES (2,1, false),
+INSERT INTO user_reaction ("user_id", "content_id", "isLiked") VALUES (2,1, false),
 (1,1, true);
