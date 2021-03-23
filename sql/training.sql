1ec71976174aa1563457d6eb90e68197f9fc4bec
Kyrylo Rozumovskyi <nortforce@gmail.com>
2021-03-23 13:36:52 +0200

    Add aggregation func tasks solution

--- sql/training.sql
+++ sql/training.sql
@@ -9,12 +9,12 @@ CREATE TABLE users (
   "isMale" boolean NOT NULL,
   "birthday" date NOT NULL CHECK ("birthday" < current_date AND "birthday" > '1900-1-1'),
   "height" numeric(3,2) NOT NULL CHECK ("height" > 0.2 AND "height" < 3),
-  weight numeric(5,2) NOT NULL CHECK (weight > 0 AND weight < 500),
+  "weight" numeric(5,2) NOT NULL CHECK ("weight" > 0 AND "weight" < 500),
   CONSTRAINT "UNQ_FULL_NAME" UNIQUE ("first_name", "last_name"),
   CONSTRAINT "CK_FULL_NAME" CHECK ("first_name" !='' AND "last_name" !='')
 );
 
-INSERT INTO users ("first_name", "last_name", "email", "isMale", "birthday", "height", weight) VALUES 
+INSERT INTO users ("first_name", "last_name", "email", "isMale", "birthday", "height", "weight") VALUES 
 ('Test', 'Testovich', 'test@test.test', true, '1991-12-21', 1.95, 100),
 ('Archaon', 'the Everchosen', 'burntheoldworld@chaos.net', true, '1964-6-6', 2.15, 10.45),
 ('Tester', 'the Evertesting', 'tester@test.test', true, '1974-6-6', 2.15, 100),
@@ -44,3 +44,26 @@ CREATE TABLE user_reaction (
 
 INSERT INTO user_reaction ("user_id", "content_id", "isLiked") VALUES (2,1, false),
 (1,1, true);
+
+SELECT avg("height") AS "average height" FROM "users";
+SELECT avg("height") AS "average height", "is_male" FROM "users" GROUP BY "is_male";
+SELECT min("height") AS "min height", "is_male" FROM "users" GROUP BY "is_male";
+
+SELECT 
+min("height") AS "min height", 
+avg("height") AS "avg height", 
+max("height") AS "max height", 
+"is_male" 
+FROM "users" GROUP BY "is_male";
+
+SELECT count(*) 
+FROM "users"
+WHERE "birthday" = '1970-1-1';
+
+SELECT count(*)
+FROM "users"
+WHERE "first_name" = 'Alexis';
+
+SELECT count(*)
+FROM "users"
+WHERE extract(year from age("birthday")) BETWEEN 20 AND 30;
