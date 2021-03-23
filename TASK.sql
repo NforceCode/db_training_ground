b6134879cfb8b8e3632a98e2c14ab2676c1ec5ab
Kyrylo Rozumovskyi <nortforce@gmail.com>
2021-03-23 12:35:02 +0200

    Fix tasks

--- TASK.sql
+++ TASK.sql
@@ -6,18 +6,23 @@ DROP TABLE "position";
 
 CREATE TABLE "workers"(
   "id" serial PRIMARY KEY,
-  "birthday" date CHECK("birthday" > '1930-1-1'),
+  "birthday" date NOT NULL DEFAULT current_date - (integer '365' * 19) CHECK(
+    "birthday" > '1930-1-1'
+    AND "birthday" < current_date
+  ),
   "name" varchar(92) NOT NULL CHECK ("name" != ''),
-  "salary" int NOT NULL CHECK ("salary" > 0)
+  "salary" numeric(9,2) NOT NULL CHECK ("salary" > 0)
 );
 
 -- Задачи на INSERT
 
 -- Добавьте нового работника Никиту, 90го года, зарплата 300$.
-INSERT INTO "workers" ("name", "birthday", "salary") VALUES ('Nikita', '1990-4-4', 300);
+INSERT INTO "workers" ("name", "birthday", "salary") 
+VALUES ('Nikita', '1990-4-4', 300);
 
 -- Добавьте нового работника Светлану с зарплатой 1200$.
-INSERT INTO "workers" ("name", "birthday", "salary") VALUES ('Svetlana',NULL, 1200);
+INSERT INTO "workers" ("name", "salary") 
+VALUES ('Svetlana', 1200);
 
 -- Добавьте двух новых работников одним запросом: Ярослава с зарплатой 1200$ и годом 80го, Петра с зарплатой 1000$ и 93 года.
 INSERT INTO "workers" ("name", "birthday", "salary") 
@@ -29,7 +34,7 @@ VALUES ('Vasya','1980-5-7', 150),
 ('Nina', '1993-8-11',1500),
 ('Anna', '1970-2-2', 1100),
 ('Fedot', '1997-5-1', 300),
-('Timur', '1956-12-31', 2500);
+('Timur', '1956-12-31', 500);
 
 -- Задачи на UPDATE
 
@@ -45,7 +50,7 @@ WHERE "id" = 4
 RETURNING *;
 
 -- Всем, у кого зарплата 500$ сделайте ее 700$.
-UPDATE "workers" SET "salary" = 700 WHERE "salary" < 500;
+UPDATE "workers" SET "salary" = 700 WHERE "salary" = 500;
 
 -- Работникам с id больше 2 и меньше 5 включительно поставьте год 99.
 UPDATE "workers" 
@@ -64,7 +69,7 @@ RETURNING *;
 -- Задачи на SELECT
 
 -- Выбрать работника с id = 3.
-SELECT * FROM "workers" WHERE "id" =3;
+SELECT * FROM "workers" WHERE "id" = 3;
 
 -- Выбрать работников с зарплатой более 400$.
 SELECT * FROM "workers" WHERE "salary" > 400;
@@ -75,7 +80,8 @@ SELECT * FROm "workers" WHERE "salary" != 500;
 
 -- Узнайте зарплату и возраст Жени.
 
-SELECT ("salary", "birthday") FROM "workers" WHERE "name" = 'Zhenya';
+SELECT "salary", extract(year from age("birthday")) AS "age" 
+FROM "workers" WHERE "name" = 'Zhenya';
 
 -- Выбрать работников с именем Петя.
 
@@ -89,14 +95,19 @@ SELECT * FROM "workers" WHERE "name" != 'Petr';
 -- Выбрать всех работников в возрасте 27 лет или с зарплатой 1000$.
 
 SELECT * FROM "workers" 
-WHERE extract(year from age("birthday")) = 27  OR "salary" = 1000;
+WHERE extract(year from age("birthday")) = 27  
+OR "salary" = 1000;
 
 -- Выбрать работников в возрасте от 25 (не включительно) до 28 лет (включительно).
 
-SELECT * FROM "workers" WHERE (extract(year from age("birthday")) BETWEEN 26 AND 28 );
+SELECT * FROM "workers" 
+WHERE (extract(year from age("birthday")) BETWEEN 26 AND 28 );
 
 -- Выбрать всех работников в возрасте от 23 лет до 27 лет или с зарплатой от 400$ до 1000$.
 SELECT * FROM "workers" 
+WHERE extract(year from age("birthday")) BETWEEN 23 AND 27 
+OR "salary" BETWEEN 400 AND 1000 ;
+
 --Выбрать всех работников в возрасте 27 лет или с зарплатой не равной 400$.
 
 SELECT * FROM "workers" 
@@ -107,16 +118,16 @@ OR "salary" != 400;
 
 -- Удалите работника с id=7.
 
-DELETE FROM "workers" WHERE "id" =7 RETURNING *;
+DELETE FROM "workers" WHERE "id" = 7 RETURNING *;
 
 -- Удалите Колю.
 
-DELETE FROM "workers" WHERE "name" ='Nikolay' RETURNING *;
+DELETE FROM "workers" WHERE "name" = 'Nikolay' RETURNING *;
 
 -- Удалите всех работников, у которых возраст 23 года.
 
-
-DELETE FROM "workers" WHERE extract (year from age("birthday")) = 23 RETURNING *;
+DELETE FROM "workers" 
+WHERE extract (year from age("birthday")) = 23 RETURNING *;
 
 --SELECT "id","name", extract (year from age("birthday")) as "full years" FROM "workers" ORDER By "id" ASC;
 
@@ -134,13 +145,13 @@ VALUES
 ('Coder'),
 ('Intern');
 
+-- Еще немного не понимаю как правильно связующие таблицы именовать
 CREATE TABLE "position_to_worker"(
 -- не уверен что тут лучше сделать ключом а что просто уникальным значением
-  "worker_id" int UNIQUE REFERENCES "workers" ("id"),
+  "worker_id" int PRIMARY KEY REFERENCES "workers" ("id"),
   "position_id" int  REFERENCES "position" ("id"),
-  PRIMARY KEY ("worker_id", "position_id")
+  CONSTRAINT "UQ_WORKER_POSITION" UNIQUE ("worker_id", "position_id")
 );
-
 
 INSERT INTO "position_to_worker" ("worker_id", "position_id") 
 VALUES 
@@ -149,6 +160,3 @@ VALUES
 (3,3),
 (5,4),
 (4,4);
-
-
-
